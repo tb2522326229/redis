201d3b06ffc7a745d412119bb685f88f67ac4413
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-10-25 20:52:36 +0800

    创建连接池链接redis，效果比直接访问要好，性能更高

--- redis_base/src/cn/yzc/demo1/Demo1.java
+++ redis_base/src/cn/yzc/demo1/Demo1.java
@@ -3,6 +3,8 @@ package cn.yzc.demo1;
 import org.junit.Test;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class Demo1 {
 
@@ -11,5 +13,21 @@ public class Demo1 {
 		Jedis jedis = new Jedis("192.168.218.129", 6379);
 	//	jedis.set("name", "李四");
 		System.out.println(jedis.get("name"));
+	}
+	
+	/*
+	 * jedis连接池
+	 * */
+	@Test
+	public void run2(){
+		//1、设置连接池的配置对象
+		JedisPoolConfig config = new JedisPoolConfig();
+		//2、设置连接池对象
+		JedisPool pool = new JedisPool(config,"192.168.218.129", 6379);
+		//3、从池中获取连接对象
+		Jedis jedis = pool.getResource();
+		System.out.println(jedis.get("name"));
+		//4、连接归还池中
+		jedis.close();
 	}
 }
